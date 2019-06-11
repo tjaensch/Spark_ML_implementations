54f0f4c1428295ee03b736479cd2f61e07cd8268
604386 <thomasjaensch@C02YJ0Q5JGH7.local>
2019-06-11 15:21:06 -0400

    Added more test commands

--- src/main/scala/DataFrames/DataFramesOverview.scala
+++ src/main/scala/DataFrames/DataFramesOverview.scala
@@ -14,11 +14,46 @@ object App {
       .master("local[*]")
       .getOrCreate()
 
+    // Read in file
     val df = spark.read.option("header","true").option("inferSchema","true").csv("src/main/scala/DataFrames/CitiGroup2006_2008.csv")
 
+    // First 5 lines
     for(line <- df.head(5)){
       println(line)
     }
+
+    println("\n")
+
+    // Get column names
+    for(line <- df.columns){
+      println(line)
+    }
+
+    println("\n")
+
+    // Print Schema
+    df.printSchema()
+
+    // Describe DataFrame Numerical Columns
+    df.describe()
+
+    // Select columns .transform().action()
+    df.select("Volume").show()
+
+    // Multiple Columns
+    df.select("Date","Close").show(2)
+
+    // Creating New Columns
+    val df2 = df.withColumn("HighPlusLow",df("High")-df("Low"))
+    // Show result
+    df2.columns
+    df2.printSchema()
+
+    // Recheck Head
+    df2.head(5)
+
+    // Renaming Columns (and selecting some more)
+    df2.select(df2("HighPlusLow").as("HPL"),df2("Close")).show()
 
 
   }
