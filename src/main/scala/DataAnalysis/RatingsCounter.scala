3b61de865cc9bb0698ec3e1a6257ad705393a657
604386 <thomasjaensch@C02YJ0Q5JGH7.local>
2019-06-25 14:08:24 -0400

    Added RatingsCounter.scala plus dataset

---
+++ src/main/scala/DataAnalysis/RatingsCounter.scala
@@ -0,0 +1,33 @@
+package DataAnalysis
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkContext
+
+/** Count up how many of each star rating exists in the MovieLens 100K data set. */
+object RatingsCounter {
+
+  /** Our main function where the action happens */
+  def main(args: Array[String]) {
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    // Create a SparkContext using every core of the local machine, named RatingsCounter
+    val sc = new SparkContext("local[*]", "RatingsCounter")
+
+    // Load up each line of the ratings data into an RDD
+    val lines = sc.textFile("src/main/scala/DataAnalysis/ml-100k/u.data")
+
+    // Convert each line to a string, split it out by tabs, and extract the third field.
+    // (The file format is userID, movieID, rating, timestamp)
+    val ratings = lines.map(x => x.toString().split("\t")(2))
+
+    // Count up how many times each value (rating) occurs
+    val results = ratings.countByValue()
+
+    // Sort the resulting map of (rating, count) tuples
+    val sortedResults = results.toSeq.sortBy(_._1)
+
+    // Print each result on its own line.
+    sortedResults.foreach(println)
+  }
+}
